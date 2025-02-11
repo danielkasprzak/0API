66959baf353593fdd4b3501b852e63808c5a30c9
Daniel Kasprzak <gamecast217@gmail.com>
2025-02-11 20:00:44 +0100

    Changed User and Statistics models
    - added LastActivity to User model
    - removed TotalUsers and ActiveUsers from Statistics model

---
+++ Models/Statistics.cs
@@ -0,0 +1,9 @@
+﻿namespace _0API.Models
+{
+    public class Statistics
+    {
+        public int Id { get; set; }
+        public int TotalPentests { get; set; }
+        public int TotalOpens { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace _0API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string HWID { get; set; }
+        public int IsBanned { get; set; }
+        public DateTime LastActivity { get; set; }
+    }
+}
