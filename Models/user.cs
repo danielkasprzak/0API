cf7416a49b25659510cd8b2b985cb75ecf4b6fa2
Daniel Kasprzak <daniel.kasprzak2602@gmail.com>
2025-02-11 20:01:21 +0100

    Deleted old, bad named user model

--- Models/user.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace _0API.Models
-{
-    public class user
-    {
-        public int id { get; set; }
-        public string hwid { get; set; }
-        public int banned { get; set; }
-    }
-}
