06a3cb4ab5f828717719c039100edd1542247c45
Daniel Kasprzak <gamecast217@gmail.com>
2025-02-11 20:02:08 +0100

    Changed context directory to Data

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using _0API.Models;
+
+namespace _0API.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Statistics> Statistics { get; set; }
+    }
+}
